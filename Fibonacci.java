00fe8c787eb0f4a5585fae9faa42feb1f0bc65ae
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-16 16:30:41 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+package Recursion;
+import java.util.Scanner;
+
+public class Fibonacci {
+	
+	static int Fib(int n)
+	{
+		int result = 0 , i = 1;
+		int a = 0 , b = 1 ;
+		while(i <= n)
+		{
+			System.out.print(a+" ");
+			result = a + b ;
+			a = b ;
+			b = result ; 
+			i++ ;	
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the fibonacci range to be displayed:");
+		int n = sc.nextInt();
+		Fib(n);
+		sc.close();
+	}
+
+}
