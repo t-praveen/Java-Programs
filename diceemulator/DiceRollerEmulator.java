1d8c865b09c89010bb6e450d519ef6a428185901
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-19 13:36:01 +0530

    Add files via upload

---
+++ diceemulator/DiceRollerEmulator.java
@@ -0,0 +1,36 @@
+package diceemulator;
+import java.util.Scanner;
+import java.util.Random;
+
+public class DiceRollerEmulator {
+	
+	public static void main(String args[])
+	{
+		System.out.print("Enter the number of dice:");
+		Scanner sc = new Scanner(System.in);
+		int numberofDice = sc.nextInt();
+		
+		Random ranNum = new Random();
+		
+		System.out.print("Hey ! You rolled:");
+		
+		int total = 0 ; 
+		int randomNumber =0 ;
+		for(int i = 0 ; i < numberofDice ; i++)
+		{
+			randomNumber = ranNum.nextInt(6) + 1 ;
+			total = total + randomNumber ;
+			System.out.print(randomNumber);
+			System.out.print(" ");
+		}
+		System.out.println(" ");
+		System.out.println("Total: "+ total);
+		sc.close();
+		
+		
+		
+		
+		
+	}
+
+}
