433b65ac12b33ac8657f4c12ef7e0e10b2246848
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-20 13:34:32 +0530

    Add files via upload

---
+++ strong/StrongNumber.java
@@ -0,0 +1,47 @@
+package strong;
+import java.util.Scanner;
+
+public class StrongNumber {
+	
+	static int SN(int num)
+	{
+		int i , p , r ;
+		int sum = 0 ;
+		int x ;
+		x = num ;
+		while(num != 0)
+		{
+			i = 1 ;
+			p = 1 ;
+			r = num % 10 ;
+			while(i <= r)
+			{
+				p = p * i ;
+				i++;
+			}
+			sum = sum + p ;
+			num = num / 10 ;
+			}
+		if(sum == x)
+		{
+			System.out.println(x+" is a Strong Number .");
+		}
+		else
+		{
+			System.out.println(x+" is not a Strong number .");
+		}
+		return x ;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number to check wether it is strong or not : ");
+		int num = sc.nextInt();
+		SN(num);
+
+		
+		
+	}
+
+}
