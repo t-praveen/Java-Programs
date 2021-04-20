cd725a88f2da616feb2b16d5d8a185b61edee61c
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-20 14:30:35 +0530

    Add files via upload

---
+++ sunny/SunnyNumber.java
@@ -0,0 +1,31 @@
+package sunny;
+import java.util.Scanner;
+
+public class SunnyNumber {
+	
+	static int SN(int n)
+	{
+		double x ; // for sqrt(num + 1) 
+		x = Math.sqrt(n + 1);
+		if((int)x == x)
+		{
+			System.out.println(n+" is a Sunny Number . ");
+		}
+		else
+		{
+			System.out.println(n+" is not a Sunny Number . ");
+		}
+		return n ; 
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number to check wether it is Sunny or not : ");
+		int n = sc.nextInt();
+		SN(n);
+		sc.close();
+
+	}
+
+}
