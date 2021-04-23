0d1087d925f9a40774a1e238099b7e4a2035fdb5
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-23 14:18:15 +0530

    Add files via upload

---
+++ changingArraySize/ChangingArraySizeUsingCustomApproach.java
@@ -0,0 +1,31 @@
+package changingArraySize;
+import java.util.Scanner;
+
+public class ChangingArraySizeUsingCustomApproach {
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter how many elements to be entered in array : ");
+		int n = sc.nextInt();
+		System.out.println("Enter the elements into the array : ");
+		int a[] = new int[n];
+		for(int i = 0 ; i < n ; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("Array before ReSize: ");
+        for(int i = 0; i < a.length; i++) 
+        	System.out.println(a[i]);
+		int[] temp = new int[6];
+        int length = a.length;
+        for(int j = 0; j < length; j++)
+            temp[j] = a[j];
+        a = temp;
+        System.out.println("Array after ReSize: ");
+        for(int k = 0; k < a.length; k++) 
+            System.out.println(a[k]);
+       sc.close();
+        
+	}
+}
---
+++ changingArraySize/ChangingArraySizeUsingSystemArrayCopy.java
@@ -0,0 +1,34 @@
+package changingArraySize;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ChangingArraySizeUsingSystemArrayCopy {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter how many elements to be entered in array : ");
+		int n = sc.nextInt();
+		System.out.println("Enter the elements into the array : ");
+		int a[] = new int[n];
+		for(int i = 0 ; i < n ; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("Array before ReSize: ");
+        for(int i = 0; i < a.length; i++) 
+        	System.out.println(a[i]);
+        a = Arrays.copyOf(a, 6);
+        a[4] = 11;
+        a[5] = 55;
+ 
+        System.out.println("Array after ReSize: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+   }
+
+}
+
+
