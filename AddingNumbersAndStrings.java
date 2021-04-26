45fdcfe6755c95e38066f1595fc04d2a32ec6298
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-26 14:52:07 +0530

    Add files via upload

---
+++ AddingNumbersAndStrings.java
@@ -0,0 +1,25 @@
+package strings;
+import java.util.Scanner;
+public class AddingNumbersAndStrings {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 20;
+		int z = x + y;  
+		System.out.println(z+"\t If you add two numbers, the result will be a number . "); // add two numbers, the result will be a number .
+		
+		String s1 = "10";
+		String s2  = "20";
+		String S3 = s1 + s2;  
+		System.out.println(S3+"\t If you add two strings, the result will be a string concatenation . "); //  add two strings, the result will be a string concatenation .
+		
+		
+		String a = "10";
+		int b  = 20;
+		String s4 = a + b;  
+		System.out.println(s4+"\t If you  add a number and a string, the result will be a string concatenation ."); //  add a number and a string, the result will be a string concatenation .
+		
+	}
+
+}
