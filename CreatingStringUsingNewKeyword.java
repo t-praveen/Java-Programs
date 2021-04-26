12d056dcd66a62648f13538b623883cfae9f9855
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-26 14:51:40 +0530

    Add files via upload

---
+++ CreatingStringUsingNewKeyword.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class CreatingStringUsingNewKeyword {
+
+	public static void main(String[] args) {
+
+		String name = new String("My name is Praveen . ");
+		System.out.println(name);
+		
+	}
+
+}
