40f6b5386bd08d24b20999acbfae6705c829c6a2
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-22 15:09:01 +0530

    Add files via upload

---
+++ Add6.java
@@ -0,0 +1,11 @@
+class Add6
+{
+    public static void main(String args[])
+    {
+        int i,k,j;
+        i=5;
+        j=15;
+        k=i+j;
+        System.out.printf("The sum of %d and %d is %d",i,j,k);
+    }
+}
