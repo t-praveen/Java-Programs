a9a83bb0b9d89d457464c81c7414eec3e6b1ccd9
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-22 15:11:55 +0530

    Add files via upload

---
+++ Loop11.java
@@ -0,0 +1,12 @@
+class Loop11
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
