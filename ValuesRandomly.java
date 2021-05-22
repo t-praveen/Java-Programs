73c5d1921460bed75c64ee34ebbb13a1a3be7eea
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-22 15:10:25 +0530

    Add files via upload

---
+++ ValuesRandomly.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+class ValuesRandomly
+{
+    public static void main(String args[])
+    {
+        Random r=new Random();
+        int a[]=new int[20];
+        for(int i=0;i<a.length;i++)
+            a[i]=r.nextInt(100);//To print random values less than 100//
+        for(int i:a)
+            System.out.println(i);
+    }
+}
