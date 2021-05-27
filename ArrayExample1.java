75832d3d1496d756a0d11a5da641d351eb8ea83b
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-27 16:05:25 +0530

    Add files via upload

---
+++ ArrayExample1.java
@@ -0,0 +1,14 @@
+class ArrayExample1
+{
+    public static void main(String args[])
+    {
+        int a[]=new int[5];
+        a[0]=1;
+        a[1]=2;
+        a[2]=10;
+        a[3]=20;
+        a[4]=100;
+        System.out.println(a);//Here it will print the hashcode because arrays in java is objects.And each object has hashcode//
+
+    }
+}
