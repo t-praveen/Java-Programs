d39f98a8a04e456ffd0a35a46b1eecd00d65ef04
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-27 16:03:21 +0530

    Add files via upload

---
+++ ArrayExample3.java
@@ -0,0 +1,15 @@
+class ArrayExample3
+{
+    public static void main(String args[])
+    {
+        int a[]=new int[5];
+        a[0]=1;
+        a[1]=2;
+        a[2]=10;
+        a[3]=20;
+        a[4]=100;
+        for(int i=0;i<5;i++)
+            System.out.println(a[i]);
+
+    }
+}
