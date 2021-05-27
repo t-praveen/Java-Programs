5f69cf85b420839d067acd8da658672bcd29aeaa
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-27 16:04:14 +0530

    Add files via upload

---
+++ StaticDemo1.java
@@ -0,0 +1,15 @@
+class StaticDemo1
+{
+    static
+    {
+        System.out.println("Hello");
+    }
+    public static void main(String args[])
+    {
+        System.out.println("Have a good day");
+    }
+    static
+    {
+        System.out.println("Bye");
+    }
+}
