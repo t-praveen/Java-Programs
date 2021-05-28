a4d764d536072451713b03844b7717557aac33c4
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-28 14:10:27 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,21 @@
+class SwitchCase
+{
+    public static void main(String args[])
+    {
+        int i=5;
+        switch(i)
+        {
+            case 0:System.out.println("ZERO");
+                   break;
+            case 1:System.out.println("ONE");
+                   break;
+            case 2:System.out.println("TWO");
+                   break;
+            case 3:System.out.println("THREE");
+                   break;
+            case 4:System.out.println("FOUR");
+                   break;
+            default:System.out.println("SOME NUMBER");
+        }
+    }
+}
