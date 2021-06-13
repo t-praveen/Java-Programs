5999d5ea7f09cc6c0d80fe021d2bc6335e978061
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-13 16:27:37 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,11 @@
+class Add1
+{
+    public static void main(String args[])
+    {
+        int num1=20;
+        int num2=20;
+        num2=30;
+        num1=10;
+        System.out.println(num1+num2);
+    }
+}
