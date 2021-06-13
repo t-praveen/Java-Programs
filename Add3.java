ab8154eef33aa62244041b4f934c1b2d205b2d55
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-13 16:26:04 +0530

    Add files via upload

---
+++ Add3.java
@@ -0,0 +1,9 @@
+class Add3
+{
+     public static void main(String args[])
+    {
+            float num1=15.0f;
+            float num2=15.0f;
+            System.out.println(num1+num2);
+    }
+}   
