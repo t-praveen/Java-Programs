f283c05c9358e3a34abfc087bd0956a131056e7b
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-13 16:29:22 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+class Constructor
+{
+    public static void main(String args[])
+    {
+        Abc obj=new Abc();
+    }
+}
+class Abc
+{
+    public Abc()//Default Constructor//
+    {
+        System.out.println("Hi, I am a Constructor.");
+    }
+}
