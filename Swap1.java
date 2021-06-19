3593d5d8e611a568ff1a4d52f819b237176b3111
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-19 13:04:52 +0530

    Add files via upload

---
+++ Swap1.java
@@ -0,0 +1,17 @@
+class Swap1
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=4;
+        int temp;
+        System.out.println("Before Swapping, the numbers are:--");
+        System.out.println("a:"+a+"\n"+"b:"+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping, the numbers are:--");
+        System.out.println("a:"+a+"\n"+"b:"+b);
+
+    }
+}
