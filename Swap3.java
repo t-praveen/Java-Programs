899302b259d5698c1aa27612bc4694bfeb7b116d
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-19 13:05:29 +0530

    Add files via upload

---
+++ Swap3.java
@@ -0,0 +1,16 @@
+class Swap3
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=4;
+        System.out.println("Before Swapping, the numbers are:--");
+        System.out.println("a:"+a+"\n"+"b:"+b);
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("After Swapping, the numbers are:--");
+        System.out.println("a:"+a+"\n"+"b:"+b);
+
+    }
+}
