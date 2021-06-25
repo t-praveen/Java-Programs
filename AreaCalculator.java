b042f4b06302b8e4515081dafdd137ceb0ad9955
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-25 20:51:13 +0530

    Add files via upload

---
+++ AreaCalculator.java
@@ -0,0 +1,35 @@
+package Recursion;
+import java.util.Scanner ;
+
+public class AreaCalculator {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in) ;
+		System.out.print("Enter Radius : ");
+		double radius = sc.nextDouble();
+		double result1 = area(radius);
+		System.out.println("The Area of the circle is :  "+result1);
+		System.out.print("Enter Length : ");
+		double x = sc.nextDouble();
+		System.out.print("Enter Breadth : ");
+		double y = sc.nextDouble();
+		double result2 = area(x ,y);
+		System.out.println("The Area of the Rectangle is : "+result2);
+		sc.close();
+
+	}
+	public static double area(double radius)
+	{
+		if(radius < 0)
+			return -1.0 ;
+		return Math.PI * radius * radius ;
+	}
+	public static double area(double x , double y)
+	{
+		if (x < 0 || y < 0)
+			return -1.0 ;
+		return x * y ;
+	}
+
+}
